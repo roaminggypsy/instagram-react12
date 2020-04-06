bd77552aa4be0e423b7dff98a386d113c6cd050a
tesseradecades <nwe8978@rit.edu>
2020-04-06 07:16:43 -0400

    not found page complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import EditProfilePage from "./pages/edit-profile";
+import ExplorePage from './pages/explore';
+import FeedPage from './pages/feed';
+import LoginPage from './pages/login';
+import NotFoundPage from './pages/not-found';
+import PostPage from './pages/post';
+import ProfilePage from './pages/profile';
+import SignUpPage from './pages/signup';
 
 function App() {
-  return <div>app</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={FeedPage}/>
+        <Route path="/explore" component={ExplorePage}/>
+        <Route exact path="/:username" component={ProfilePage}/>
+        <Route path="/accounts/edit" component={EditProfilePage}/>
+        <Route path="/accounts/emailsignup" component={SignUpPage}/>
+        <Route path="/accounts/login" component={LoginPage}/>
+        <Route exact path="/p/:postId" component={PostPage}/>
+        <Route path="*" component={NotFoundPage}/>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
